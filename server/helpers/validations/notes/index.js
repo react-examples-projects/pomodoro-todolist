2ccdfc2807c827ca7b181ca820206c2a82c142a1
Libardo <libardojesusrengifo129@gmail.com>
2021-08-29 20:51:32 -0400

    dev: remove note validation

--- server/helpers/validations/notes/index.js
+++ server/helpers/validations/notes/index.js
@@ -1,15 +1,19 @@
 const yup = require("yup");
+const idSchema = yup
+  .string()
+  .typeError("El indenficador debe ser un ObjectId")
+  .required("El identificador es requerido");
 
 const createNoteSchema = yup.object({
   body: yup.object({
     title: yup
       .string()
-      .max(100)
+      .max(100, "Máximo 100 carácteres permitidos para el título de la nota")
       .typeError("El título de la nota debe ser una cadena")
       .required("El título de la nota es obligatorio"),
     content: yup
       .string()
-      .max(500)
+      .max(500, "Máximo 100 carácteres permitidos para el contenido de la nota")
       .typeError("El contenido de la nota debe ser una cadena")
       .required("El contenido de la nota es obligatorio"),
     tags: yup
@@ -24,6 +28,39 @@ const createNoteSchema = yup.object({
   }),
 });
 
+const updateNoteSchema = yup.object({
+  body: yup.object({
+    title: yup
+      .string()
+      .max(100, "Máximo 100 carácteres permitidos para el título de la nota")
+      .typeError("El título de la nota debe ser una cadena"),
+    content: yup
+      .string()
+      .max(500, "Máximo 100 carácteres permitidos para el contenido de la nota")
+      .typeError("El contenido de la nota debe ser una cadena"),
+    tags: yup
+      .array()
+      .of(
+        yup.object().shape({
+          title: yup.string(),
+          color: yup.string(),
+        })
+      )
+      .typeError("Las etiquetas deben ser un arreglo de cadenas"),
+  }),
+  params: yup.object({
+    id: idSchema,
+  }),
+});
+
+const removeNoteSchema = yup.object({
+  params: yup.object({
+    id: idSchema,
+  }),
+});
+
 module.exports = {
   createNoteSchema,
+  updateNoteSchema,
+  removeNoteSchema,
 };
--- server/routers/notes/index.js
+++ server/routers/notes/index.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const noteController = require("../../controllers/noteController");
 
 const validate = require("../../helpers/validations/validate");
-const { createNoteSchema } = require("../../helpers/validations/notes");
+const {
+  createNoteSchema,
+  updateNoteSchema,
+  removeNoteSchema,
+} = require("../../helpers/validations/notes");
 
 router.get("/", noteController.getNotes);
 router.post("/", validate(createNoteSchema), noteController.createNote);
-router.put("/:id", noteController.updateNote);
-router.delete("/:id", noteController.removeNote);
+router.put("/:id", validate(updateNoteSchema), noteController.updateNote);
+router.delete("/:id", validate(removeNoteSchema), noteController.removeNote);
 
 module.exports = router;
